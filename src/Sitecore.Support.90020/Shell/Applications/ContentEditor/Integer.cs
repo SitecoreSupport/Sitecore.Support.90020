949161d5b1195707857635ca52fd5c7c9d08aa21
DK\mao <mao@sitecore.net>
2018-10-26 15:03:38 +0300

    Add default Integer formatter implementation.

---
+++ src/Sitecore.Support.90020/Shell/Applications/ContentEditor/Integer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+
+namespace Sitecore.Support.Shell.Applications.ContentEditor
+{
+  public class Integer : Sitecore.Shell.Applications.ContentEditor.Integer
+  {
+    protected override string FormatNumber(string value)
+    {
+      if (value.Length == 0)
+      {
+        return string.Empty;
+      }
+
+      long result;
+      if (!long.TryParse(value, NumberStyles.Integer, CultureInfo.InvariantCulture, out result))
+      {
+        return value;
+      }
+
+      return result.ToString("#", GetCultureInfo());
+    }
+  }
+}
